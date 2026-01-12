298fcbfd5ae70d49f6bdf3373234409ba7a54434
Daniel Starnes <dstarnes261@gmail.com>
2026-01-12 15:19:03 -0600

    Initial Commit

---
+++ BrainTrainer/BrainTrainerApp.swift
@@ -0,0 +1,17 @@
+//
+//  BrainTrainerApp.swift
+//  BrainTrainer
+//
+//  Created by Daniel Starnes on 1/12/26.
+//
+
+import SwiftUI
+
+@main
+struct BrainTrainerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
