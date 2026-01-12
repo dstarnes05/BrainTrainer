3827338f4764d3b4bfd4202aff36d5e48f206892
Daniel Starnes <dstarnes261@gmail.com>
2026-01-12 17:27:36 -0600

    updated core logic to present correct scoreTitle created functions to reset game & go to next question; created 2 alerts to present score after each question and at end of game

--- BrainTrainer/ContentView.swift
+++ BrainTrainer/ContentView.swift
@@ -9,11 +9,15 @@ import SwiftUI
 
 struct ContentView: View {
     
-    let moves = ["Rock", "Paper", "Scissors"]
+    @State private var moves = ["Rock", "Paper", "Scissors"]
     @State private var currentChoice = Int.random(in: 0...2)
     @State private var playerScore = 0
     @State private var questionsAsked = 0
     @State private var winning = Bool.random()
+    @State private var scoreTitle = ""
+    
+    @State private var showingScore = false
+    @State private var gameComplete = false
     
     struct GameButton : View {
         var text: String
@@ -23,6 +27,9 @@ struct ContentView: View {
             Button(action: { action(text) }) {
                 Text(text)
             }
+            .buttonStyle(BorderedProminentButtonStyle())
+            .padding()
+            .bold(true)
         }
     }
     
@@ -35,7 +42,8 @@ struct ContentView: View {
             
             Spacer()
             
-            Text("The app chooses \(moves[currentChoice]). You must \(winning ? "win" : "lose") this round.")
+            Text("The app chooses \(moves[currentChoice]).")
+            Text("You must \(winning ? "win" : "lose") this round.")
                 .foregroundStyle(winning ? .green : .red)
             
             Spacer()
@@ -53,51 +61,92 @@ struct ContentView: View {
             
             
         }
+        .alert(scoreTitle, isPresented: $showingScore) {
+            Button("Continue", action: appChooses)
+        } message: {
+            Text("Your score is \(playerScore).")
+        }
+        
+        .alert("Game is complete. Your total score is \(playerScore) out of 10.", isPresented: $gameComplete) {
+            Button("Start Over", action: resetGame)
+                .foregroundStyle(.red)
+        }
     }
+    
+        func appChooses(){
+            moves.shuffle()
+            currentChoice = Int.random(in: 0...2)
+            winning.toggle()
+        }
+    
+        func resetGame(){
+            gameComplete = false
+            playerScore = 0
+            questionsAsked = 0
+        }
         
         func chooseMove(move: String) {
             if winning && moves[currentChoice] == "Rock" {
                 if move == "Paper" {
                     playerScore += 1
+                    scoreTitle = "Correct!"
                 } else {
                     playerScore -= 1
+                    scoreTitle = "Incorrect."
                 }
             }
             if winning && moves[currentChoice] == "Paper" {
                 if move == "Scissors" {
                     playerScore += 1
+                    scoreTitle = "Correct!"
                 } else {
                     playerScore -= 1
+                    scoreTitle = "Incorrect."
                 }
             }
             if winning && moves[currentChoice] == "Scissors" {
                 if move == "Rock" {
                     playerScore += 1
+                    scoreTitle = "Correct!"
                 } else {
                     playerScore -= 1
+                    scoreTitle = "Incorrect."
                 }
             }
             if !winning && moves[currentChoice] == "Rock" {
                 if move == "Scissors" {
                     playerScore += 1
+                    scoreTitle = "Correct!"
                 } else {
                     playerScore -= 1
+                    scoreTitle = "Incorrect."
                 }
             }
             if !winning && moves[currentChoice] == "Paper" {
-                if move == "Scissors" {
+                if move == "Rock" {
                     playerScore += 1
+                    scoreTitle = "Correct!"
                 } else {
                     playerScore -= 1
+                    scoreTitle = "Incorrect."
                 }
             }
             if !winning && moves[currentChoice] == "Scissors" {
-                if move == "Rock" {
+                if move == "Paper" {
                     playerScore += 1
+                    scoreTitle = "Correct!"
                 } else {
                     playerScore -= 1
+                    scoreTitle = "Incorrect."
                 }
             }
+            questionsAsked += 1
+            
+            if questionsAsked == 10 {
+                gameComplete = true
+            }
+            
+            showingScore = true
         }
     }
 
